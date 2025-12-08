bef78bcc5470195d00d8fb61e3447d07a08c21df
Gowshekan <gowshekangowshekan@gmail.com>
2025-12-08 11:44:23 +0530

    async in js

---
+++ js/async.js
@@ -0,0 +1,25 @@
+/*setTimeout(() => {
+    console.log("SetTimeout Function");
+}, 0);
+//callback function and timer
+console.log("Hello world!");
+//callback hell
+//nested callback functions
+setTimeout(() => {
+   console.log("setTimeout Function 1");
+   setTimeout(()=> {
+   console.log("setTimeout Function 2")
+   //return
+   setTimeout(() =>{
+    console.log("setTimeout Function 3")
+    setTimeout(() =>{
+        console.log("setTimeout Function 4");
+    setTimeout(() =>{
+        console.log("setTimeout Function 5");
+    }, 2000);
+    }, 2000);
+    }, 2000);
+   }, 2000);
+}, 2000);*/
+
+//promise
